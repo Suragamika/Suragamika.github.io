391a1467ac1fd4b58a35d1f273b1195065e50cca
Suragamika <458972877@qq.com>
2024-12-18 22:20:25 +0800

    Site updated: 2024-12-18 22:20:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/12/18/hello-world/"];function toRandomPost(){
+var posts=["2024/12/18/hello-world/","2024/12/18/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
